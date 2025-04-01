2d2b68232df14c42adb9d777dcf4a1d534b3dfaf
Abdallah Othman <abdallah.othman@lokalise.com>
2025-04-01 16:23:34 +0200

    chore: update project configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import node from "@astrojs/node";
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: "https:/abdallahaho.com",
+	site: 'https:/abdallahaho.com',
 	adapter: node({
-		mode: "standalone",
+		mode: 'standalone',
 	}),
 	integrations: [mdx(), sitemap(), tailwind(), react()],
+	vite: {
+		resolve: {
+			alias: {
+				'@components': '/src/components',
+				'@layouts': '/src/layouts',
+				'@types': '/src/types',
+				'@lib': '/src/lib',
+				'@consts': '/src/consts',
+				'@assets': '/src/assets',
+			},
+		},
+	},
 });
